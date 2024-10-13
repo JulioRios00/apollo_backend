2f224037fcb344a5c7f9bc627637216004001707
Julio Rios <julio.rios.araujo@gmail.com>
2024-10-13 15:19:20 -0300

    adjusting category field

--- new_products/serializers.py
+++ new_products/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_display = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'color', 'category', 'price', 'promotional_price']
+        fields = ['id', 'name', 'description', 'color', 'category_display', 'price', 'promotional_price']
         read_only_fields = ['promotional_price']
+    def get_category_display(self, obj):
+        return obj.get_category_display()
