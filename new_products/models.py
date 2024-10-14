5d896c06b1b0b73ca21cff6f8b2105238896377e
Julio Rios <julio.rios.araujo@gmail.com>
2024-10-14 00:01:30 -0300

    adjusting currenc

--- new_products/models.py
+++ new_products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from decimal import Decimal
 
 PRODUCT_CATEGORIES = [
     ("portable_appliances", "Eletroportáteis"),
@@ -21,12 +22,18 @@ class Product(models.Model):
     description = models.TextField()
     color = models.CharField(max_length=25)
     category = models.CharField(max_length=60, choices=PRODUCT_CATEGORIES)
-    price = models.DecimalField(max_digits=10, decimal_places=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     
     @property
     def promotional_price(self):
         discount = DISCOUNTS_BY_CATEGORIES.get(self.category, 0)
         return float(self.price) * (1 - discount)
+    
+    def save(self, *args, **kwargs):
+        if isinstance(self.price, str):
+            clean_price = self.price.replace(".", "").replace(",", ".")
+            self.price = Decimal(clean_price)
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return self.name
